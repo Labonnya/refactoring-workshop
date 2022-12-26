bf221d140422d0f76e66e64ab5fe212c83a756ff
Labonnya <75426550+Labonnya@users.noreply.github.com>
2022-12-27 03:32:40 +0600

    Update TriviaGame.java

--- java/src/main/java/workshop/Trivia Game/TriviaGame.java
+++ java/src/main/java/workshop/Trivia Game/TriviaGame.java
@@ -44,9 +44,50 @@ public class TriviaGame {
         }
 
     }
-    wasCorrectlyAnswered() ; 
-    wrongAnswer();
+    public boolean wasCorrectlyAnswered() {
+        if (getCurrentPlayer(currentPlayer).inPenaltyBox) {
+            if (isGettingOutOfPenaltyBox) {
+                getCurrentPlayer(currentPlayer).goldCoins();
+                boolean winner = didPlayerWin();
+                updatePlayer();
+
+                return winner;
+
+            } else {
+                updatePlayer();
+                return true;
+            }
+
+        } else {
+
+            getCurrentPlayer(currentPlayer).goldCoins();
+
+            boolean winner = didPlayerWin();
+            currentPlayer++;
+            if (currentPlayer == players.size()) currentPlayer = 0;
+
+            return winner;
+        }
     }
 
+    public void updatePlayer(){
+        currentPlayer++;
+        if (currentPlayer == players.size()) currentPlayer = 0;
+    }
+
+
+    public boolean wrongAnswer() {
+        System.out.println("Question was incorrectly answered");
+        System.out.println(getCurrentPlayer(currentPlayer).getName() + " was sent to the penalty box");
+        getCurrentPlayer(currentPlayer).inPenaltyBox = true;
+
+        updatePlayer();
+        return true;
+    }
+
+    private boolean didPlayerWin() {
+        return !(getCurrentPlayer(currentPlayer).purses == 6);
+    }
+}
 
 
