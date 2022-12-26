4e73b403ebe95852587b0d7362720e404922ef3d
Labonnya <75426550+Labonnya@users.noreply.github.com>
2022-12-27 05:10:30 +0600

    Create NullResponse.java

---
+++ java/src/main/java/workshop/FizzBuzz/NullResponse.java
@@ -0,0 +1,17 @@
+package workshop;
+
+public class NullResponse implements PatternMatcher{
+
+	@Override
+	public boolean matchPattern(int number) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String response() {
+		// TODO Auto-generated method stub
+		return "";
+	}
+	
+}
