1af6b5f8c7e2e62c8f1517b3dc2697de9705d6dd
Labonnya <75426550+Labonnya@users.noreply.github.com>
2022-12-27 01:11:37 +0600

    Update PlainTextToHTMLCovert.java

--- java/src/main/java/workshop/PlaintTextToHTML/PlainTextToHTMLCovert.java
+++ java/src/main/java/workshop/PlaintTextToHTML/PlainTextToHTMLCovert.java
@@ -1,1 +1,54 @@
 
+package plainTxttoHTML;
+
+import refac.NullPrint;
+import refac.patternMatcher;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlaintextToHtmlConverter {
+
+    List<CharacterConvert> charconvert;
+
+    public PlaintextToHtmlConverter(List<CharacterConvert> chrcon)
+    {
+        this.charconvert = chrcon;
+    }
+
+        public String toHtml() throws Exception {
+            String text = read();
+            String htmlLines = basicHtmlEncode(text);
+            return htmlLines;
+        }
+
+        protected String read() throws IOException {
+            return new String(Files.readAllBytes(Paths.get("sample.txt")));
+        }
+
+        private String basicHtmlEncode(String source) {
+            int i = 0;
+            List<String> result = new ArrayList<>();
+            List<String> convertedLine = new ArrayList<>();
+
+            for(char characterToConvert : source.toCharArray())
+            {
+                for(CharacterConvert charcon: charconvert)
+                {
+                    if(charcon.checknConvert(characterToConvert))
+                    {
+                        convertedLine.add(charcon.addconverted());
+                    }
+                   
+                if (i >= source.length()) break;
+            }
+            addANewLine(convertedLine, result);
+            String finalResult = String.join("<br />", result);
+            return finalResult;
+        }
+
+
+        
